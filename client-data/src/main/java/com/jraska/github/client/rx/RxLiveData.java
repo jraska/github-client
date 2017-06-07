65d82c6f3e6c55d141aaf1e27e0a3ebe6772f312
Josef Raska <josef.raska@ocado.com>
2017-06-07 19:25:56 +0100

    Example with dummy ViewState preventing Activity to handle errors

--- client-data/src/main/java/com/jraska/github/client/rx/RxLiveData.java
+++ client-data/src/main/java/com/jraska/github/client/rx/RxLiveData.java
@@ -1,14 +1,11 @@
 package com.jraska.github.client.rx;
 
-import android.arch.lifecycle.LifecycleOwner;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.Observer;
 import android.support.annotation.Nullable;
 
 import io.reactivex.Observable;
 import io.reactivex.Single;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 
 public abstract class RxLiveData<T> extends LiveData<T> {
   public static <T> RxLiveData<T> from(Single<T> single) {
@@ -19,7 +16,6 @@ public abstract class RxLiveData<T> extends LiveData<T> {
     return new ObservableAdapter<>(observable);
   }
 
-  protected Consumer<? super Throwable> onError;
   @Nullable private Disposable subscription;
 
   RxLiveData() {
@@ -46,15 +42,6 @@ public abstract class RxLiveData<T> extends LiveData<T> {
     return this;
   }
 
-  // TODO: 07/06/17 Error handling exposes RxLiveData everywhere. Too invasive
-  // Solution is to make LiveData never fail -> LiveData<ViewState>
-  public RxLiveData<T> observe(LifecycleOwner owner, Observer<T> observer,
-                               Consumer<? super Throwable> onError) {
-    this.onError = onError;
-    observe(owner, observer);
-    return this;
-  }
-
   private void dispose() {
     if (subscription != null) {
       subscription.dispose();
@@ -77,11 +64,7 @@ public abstract class RxLiveData<T> extends LiveData<T> {
 
     @Override
     Disposable subscribe() {
-      if (onError == null) {
-        return single.subscribe(this::setValueInternal);
-      } else {
-        return single.subscribe(this::setValueInternal, onError);
-      }
+      return single.subscribe(this::setValueInternal);
     }
   }
 
@@ -93,11 +76,7 @@ public abstract class RxLiveData<T> extends LiveData<T> {
     }
 
     @Override Disposable subscribe() {
-      if (onError == null) {
-        return observable.subscribe(this::setValueInternal);
-      } else {
-        return observable.subscribe(this::setValueInternal, onError);
-      }
+      return observable.subscribe(this::setValueInternal);
     }
   }
 }
