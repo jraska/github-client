69972e9bac275db33167b9bcd575fd330fed560c
Josef Raska <josef.raska@gmail.com>
2017-06-16 00:51:58 +0100

    Add Todo for Robolectric test

--- client/src/test/java/com/jraska/github/client/ui/RepoDetailActivityTest.java
+++ client/src/test/java/com/jraska/github/client/ui/RepoDetailActivityTest.java
@@ -1,6 +1,7 @@
 package com.jraska.github.client.ui;
 
 import com.jraska.github.client.BuildConfig;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
@@ -9,4 +10,8 @@ import org.robolectric.annotation.Config;
 @Config(constants = BuildConfig.class, sdk = 21)
 public class RepoDetailActivityTest {
 
+  @Test
+  public void whenRepoLoaded_repoSetProperly() {
+    // TODO: #21 Implement test with mocked ViewModel
+  }
 }
