293f63a4b85ffa73d13d1dbebf78f122afbef934
Josef Raska <josef.raska@ocado.com>
2017-06-21 20:33:03 +0100

    Users frlow test with testing framework setup

---
+++ client/src/androidTest/java/com/jraska/github/client/FakeFirebaseModule.java
@@ -0,0 +1,22 @@
+package com.jraska.github.client;
+
+import android.content.Context;
+
+import com.jraska.github.client.analytics.EventAnalytics;
+import com.jraska.github.client.logging.CrashReporter;
+
+import static org.mockito.Mockito.mock;
+
+public class FakeFirebaseModule extends FirebaseModule {
+  @Override EventAnalytics eventAnalytics(Context context, Config config) {
+    return mock(EventAnalytics.class);
+  }
+
+  @Override CrashReporter firebaseCrash() {
+    return mock(CrashReporter.class);
+  }
+
+  @Override Config config() {
+    return mock(Config.class);
+  }
+}
