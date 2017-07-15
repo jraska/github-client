6f2276540d6a2a26b5b3dcad029e07a047bd73f2
Josef Raska <josef.raska@ocado.com>
2017-07-15 13:18:09 +0000

    #44 Remove unused code

--- client/src/test/java/com/jraska/github/client/TestGitHubClientApp.java
+++ client/src/test/java/com/jraska/github/client/TestGitHubClientApp.java
@@ -1,12 +1,11 @@
 package com.jraska.github.client;
 
+import android.annotation.SuppressLint;
+
 import com.jraska.github.client.http.FakeHttpComponent;
 import com.jraska.github.client.http.HttpComponent;
 
-import org.junit.Assume;
-
-import static org.hamcrest.core.Is.is;
-
+@SuppressLint("Registered") // robolectric.properties
 public final class TestGitHubClientApp extends GitHubClientApp {
   @Override protected HttpComponent httpComponent() {
     return FakeHttpComponent.create();
@@ -18,21 +17,6 @@ public final class TestGitHubClientApp extends GitHubClientApp {
   }
 
   @Override void initThreeTen() {
-  }
-
-  @Override public void onCreate() {
-    super.onCreate();
-
-//    assumeNoCI();
-  }
-
-  /**
-   * Travis cannot download Robolectric from Sonatype from some reason
-   */
-  public static void assumeNoCI() {
-    String ciValue = System.getenv("CI");
-    boolean isCi = Boolean.parseBoolean(ciValue);
-
-    Assume.assumeThat("Test should not run on CI server.", isCi, is(false));
+    // we are running on desktop
   }
 }
