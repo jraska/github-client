ad94225455341b07ccb23f8f419e432c7d6091a1
Josef Raska <6277721+jraska@users.noreply.github.com>
2018-07-16 18:32:44 +0200

    Moving TestObserver extension to separate module

--- client-data/src/test/java/com/jraska/github/client/TestObserver.kt
+++
@@ -1,8 +0,0 @@
-package com.jraska.github.client
-
-import android.arch.lifecycle.LiveData
-import com.jraska.livedata.TestObserver
-
-fun <T> LiveData<T>.test(): TestObserver<T> {
-  return TestObserver.test(this)
-}
