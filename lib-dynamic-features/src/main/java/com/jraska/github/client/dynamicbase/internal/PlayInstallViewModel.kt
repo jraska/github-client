149cbc6170529c47a315acfbdda0843d91fbac33
Josef Raska <josef.raska@skyscanner.net>
2019-08-22 12:24:52 +0100

    Refactoring the split install methods to have them withing ViewModel

--- lib-dynamic-features/src/main/java/com/jraska/github/client/dynamicbase/internal/PlayInstallViewModel.kt
+++ lib-dynamic-features/src/main/java/com/jraska/github/client/dynamicbase/internal/PlayInstallViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.android.play.core.splitinstall.SplitInstallManager
+import com.google.android.play.core.splitinstall.SplitInstallSessionState
 import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener
 import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
 import com.jraska.github.client.common.lazyMap
@@ -21,52 +22,24 @@ internal class PlayInstallViewModel @Inject constructor(
     return liveDataMap.getValue(moduleName)
   }
 
-  private fun startInstalling(moduleName: String): LiveData<ViewState> {
+  private fun publishError(moduleName: String) {
+    val error = RuntimeException("Error downloaded")
+    liveDataMap.getValue(moduleName).value = ViewState.Error(error)
+    featureInstaller.onFeatureInstallError(moduleName, error)
+  }
+
+  private fun publishSuccess(moduleName: String) {
+    liveDataMap.getValue(moduleName).value = ViewState.Finish
+    featureInstaller.onFeatureInstalled(moduleName)
+  }
+
+  private fun startInstalling(moduleName: String): MutableLiveData<ViewState> {
     val liveData = MutableLiveData<ViewState>()
     liveData.value = ViewState.Loading
 
     splitInstallManager.beginInstallation(moduleName)
 
-    val listener = SplitInstallStateUpdatedListener {
-      if (!it.moduleNames().contains(moduleName)) {
-        return@SplitInstallStateUpdatedListener
-      }
-
-      Timber.d(it.toString())
-
-      fun publishError() {
-        val error = RuntimeException("Error downloaded")
-        liveData.value = ViewState.Error(error)
-        featureInstaller.onFeatureInstallError(moduleName, error)
-      }
-
-      fun publishSuccess() {
-        liveData.value = ViewState.Finish(moduleName)
-        featureInstaller.onFeatureInstalled(moduleName)
-      }
-
-      when (it.status()) {
-        SplitInstallSessionStatus.REQUIRES_USER_CONFIRMATION -> {
-          /*
-    This may occur when attempting to download a sufficiently large module.
-    In order to see this, the application has to be uploaded to the Play Store.
-    Then features can be requested until the confirmation path is triggered.
-   */
-//          splitInstallManager.startConfirmationDialogForResult(it, this, CONFIRMATION_REQUEST_CODE)
-        }
-        SplitInstallSessionStatus.DOWNLOADED -> Unit
-        SplitInstallSessionStatus.DOWNLOADING -> Unit
-        SplitInstallSessionStatus.UNKNOWN -> Unit
-        SplitInstallSessionStatus.PENDING -> Unit
-        SplitInstallSessionStatus.INSTALLING -> Unit
-        SplitInstallSessionStatus.INSTALLED -> publishSuccess()
-        SplitInstallSessionStatus.FAILED -> publishError()
-        SplitInstallSessionStatus.CANCELING -> publishError()
-        SplitInstallSessionStatus.CANCELED -> publishError()
-        else -> publishError()
-      }
-    }
-
+    val listener = Listener(moduleName, this)
     splitInstallManager.registerListener(listener)
 
     return liveData
@@ -77,9 +50,45 @@ internal class PlayInstallViewModel @Inject constructor(
     super.onCleared()
   }
 
+  private fun onModuleStateUpdate(it: SplitInstallSessionState, moduleName: String) {
+    Timber.d(it.toString())
+
+    when (it.status()) {
+      SplitInstallSessionStatus.REQUIRES_USER_CONFIRMATION -> onConfirmationRequired(moduleName)
+      SplitInstallSessionStatus.DOWNLOADED -> Unit
+      SplitInstallSessionStatus.DOWNLOADING -> Unit
+      SplitInstallSessionStatus.UNKNOWN -> Unit
+      SplitInstallSessionStatus.PENDING -> Unit
+      SplitInstallSessionStatus.INSTALLING -> Unit
+      SplitInstallSessionStatus.INSTALLED -> publishSuccess(moduleName)
+      SplitInstallSessionStatus.FAILED -> publishError(moduleName)
+      SplitInstallSessionStatus.CANCELING -> publishError(moduleName)
+      SplitInstallSessionStatus.CANCELED -> publishError(moduleName)
+      else -> publishError(moduleName)
+    }
+  }
+
+  private fun onConfirmationRequired(moduleName: String) {
+    /* This may occur when attempting to download a sufficiently large module.
+    In order to see this, the application has to be uploaded to the Play Store.
+    Then features can be requested until the confirmation path is triggered.*/
+    //    splitInstallManager.startConfirmationDialogForResult(it, this, CONFIRMATION_REQUEST_CODE)
+    throw NotImplementedError()
+  }
+
   sealed class ViewState {
     object Loading : ViewState()
-    class Finish(val moduleName: String) : ViewState()
+    object Finish : ViewState()
     class Error(val error: Throwable) : ViewState()
+  }
+
+  class Listener(val moduleName: String, val viewModel: PlayInstallViewModel) : SplitInstallStateUpdatedListener {
+    override fun onStateUpdate(it: SplitInstallSessionState) {
+      if (!it.moduleNames().contains(moduleName)) {
+        return
+      }
+
+      viewModel.onModuleStateUpdate(it, moduleName)
+    }
   }
 }
