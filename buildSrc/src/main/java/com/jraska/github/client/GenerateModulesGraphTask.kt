9f099ed2c6db9f3a51ef766e5026d7fb30772f12
Josef Raska <josef.raska@skyscanner.net>
2019-10-30 20:07:08 +0100

    Fix discrepancies

--- buildSrc/src/main/java/com/jraska/github/client/GenerateModulesGraphTask.kt
+++ buildSrc/src/main/java/com/jraska/github/client/GenerateModulesGraphTask.kt
@@ -4,14 +4,11 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 open class GenerateModulesGraphTask : DefaultTask() {
-  lateinit var moduleName: String
-
   @TaskAction
   fun run() {
     val allModulesTree = DependencyTreeFactory.create(project)
-    val moduleTree = allModulesTree.subTree(moduleName)
 
-    println(moduleTree.statistics())
-    println(GraphvizGenerator.toGraphviz(moduleTree))
+    println(allModulesTree.statistics())
+    println(GraphvizGenerator.toGraphviz(allModulesTree))
   }
 }
