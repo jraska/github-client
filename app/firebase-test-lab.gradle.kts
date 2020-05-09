aff562ef4dff2a04ae46c835ae067bc645c46cc5
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-05-09 17:33:15 +0200

    Run Firebase test lab as part of check

---
+++ app/firebase-test-lab.gradle.kts
@@ -0,0 +1,40 @@
+project.afterEvaluate {
+  val gcloud = "~/gcloud/google-cloud-sdk/bin/gcloud"
+  val setupGCloudProject = tasks.register("setupGCloud", Exec::class) {
+    commandLine = "$gcloud config set project github-client-25b47".split(' ')
+  }
+
+  val setupGCloudAccount = tasks.register("setupGCloud", Exec::class) {
+    val credentialsPath = createCredentialsFile()
+    commandLine = "$gcloud auth activate-service-account --key-file $credentialsPath".split(' ')
+
+    dependsOn(setupGCloudProject)
+  }
+
+  val firebaseTestsTask = tasks.register("runInstrumentedTestsOnFirebase", Exec::class) {
+    val appApk = "${project.buildDir}/outputs/apk/debug/app-debug.apk"
+    val testApk = "${project.buildDir}/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk"
+    val device = "model=Pixel2,version=29,locale=en,orientation=portrait"
+
+    commandLine =
+      ("$gcloud " +
+        "firebase test android run " +
+        "--app $appApk " +
+        "--test $testApk " +
+        "--device $device " +
+        "--no-performance-metrics")
+        .split(' ')
+    dependsOn(project.tasks.named("assembleDebugAndroidTest"))
+    dependsOn(setupGCloudAccount)
+  }
+
+  project.tasks.named("check").configure { dependsOn(firebaseTestsTask) }
+}
+
+fun Project.createCredentialsFile(): String {
+  val credentialsPath = "$projectDir/credentials.json"
+  val credentials: String = System.getenv("GCLOUD_CREDENTIALS")
+  File(credentialsPath).writeText(credentials)
+  return credentialsPath
+}
+
