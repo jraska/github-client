43458dd858139188cd153b5b895dfaed09a7e615
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-11-14 01:34:53 +0100

    Test exists

--- app/src/androidTest/java/com/jraska/github/client/settings/SettingsTest.kt
+++ app/src/androidTest/java/com/jraska/github/client/settings/SettingsTest.kt
@@ -1,15 +1,14 @@
 package com.jraska.github.client.settings
 
-import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withSubstring
 import com.jraska.github.client.DeepLinkLaunchTest
 import com.jraska.github.client.navigation.Urls
-import org.hamcrest.CoreMatchers.not
 import org.junit.Test
 
 class SettingsTest {
@@ -20,6 +19,6 @@ class SettingsTest {
     onView(withId(R.id.console_item_container)).perform(click())
 
     onView(withSubstring(Urls.console().toString())).check(matches(isDisplayed()))
-    onView(withId(R.id.console_item_container)).check(matches(not(isDisplayed())))
+    onView(withId(R.id.console_item_container)).check(doesNotExist())
   }
 }
