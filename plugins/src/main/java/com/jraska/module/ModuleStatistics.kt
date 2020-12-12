d9b739d0ef028e90f8771a54b6e1b3150288fb9b
Josef Raska <6277721+jraska@users.noreply.github.com>
2020-12-12 15:22:54 +0100

    Direct dependencies events added

--- plugins/src/main/java/com/jraska/module/ModuleStatistics.kt
+++ plugins/src/main/java/com/jraska/module/ModuleStatistics.kt
@@ -9,6 +9,7 @@ data class ProjectStatistics(
   val prodJavaTotalLines: Int,
   val prodXmlTotalLines: Int,
   val moduleStatistics: List<ModuleStatistics>,
+  val externalDependencies: List<ModuleArtifactDependency>
 )
 
 data class ModuleStatistics(
@@ -23,6 +24,16 @@ data class FileTypeStatistics(
   val lineCount: Int,
   val fileCount: Int,
   val type: FileType
+)
+
+data class ModuleArtifactDependency(
+  val moduleName: String,
+  val type: ModuleType,
+  val fullName: String,
+  val group: String,
+  val artifact: String,
+  val version: String?,
+  val configurationName: String
 )
 
 enum class ModuleType {
--- plugins/src/main/java/com/jraska/module/extract/StatisticsGradleExtractor.kt
+++ plugins/src/main/java/com/jraska/module/extract/StatisticsGradleExtractor.kt
@@ -2,10 +2,12 @@ package com.jraska.module.extract
 
 import com.jraska.module.FileType
 import com.jraska.module.FileTypeStatistics
+import com.jraska.module.ModuleArtifactDependency
 import com.jraska.module.ModuleStatistics
 import com.jraska.module.ModuleType
 import com.jraska.module.ProjectStatistics
 import org.gradle.api.Project
+import org.gradle.api.internal.artifacts.dependencies.DefaultExternalModuleDependency
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
@@ -19,12 +21,18 @@ class StatisticsGradleExtractor() {
       .filter { it.childProjects.isEmpty() }
       .map { extractFromModule(it) }
 
+    val externalDependencies = project.rootProject
+      .subprojects
+      .filter { it.childProjects.isEmpty() }
+      .flatMap { extractDependencies(it) }
+
     return ProjectStatistics(
       modulesCount = moduleStats.size,
       apiModules = moduleStats.count { it.type == ModuleType.Api },
       appModules = moduleStats.count { it.type == ModuleType.App },
       implementationModules = moduleStats.count { it.type == ModuleType.Implementation },
       moduleStatistics = moduleStats,
+      externalDependencies = externalDependencies,
       prodKotlinTotalLines = moduleStats
         .map { module -> module.containedProdFiles.single { it.type == FileType.KOTLIN }.lineCount }
         .sum(),
@@ -37,6 +45,31 @@ class StatisticsGradleExtractor() {
         .map { module -> module.containedProdFiles.single { it.type == FileType.XML }.lineCount }
         .sum()
     )
+  }
+
+  private fun extractDependencies(module: Project): List<ModuleArtifactDependency> {
+    val configurationsToLook = setOf("api", "implementation", "testImplementation", "androidTestImplementation", "kapt", "kaptAndroidTest")
+
+    val moduleType = moduleType(module)
+    val moduleName = module.name
+
+    return module.configurations
+      .filter { configurationsToLook.contains(it.name) }
+      .flatMap { configuration ->
+        configuration.dependencies
+          .filterIsInstance<DefaultExternalModuleDependency>()
+          .map {
+            ModuleArtifactDependency(
+              moduleName = moduleName,
+              type = moduleType,
+              group = it.group!!,
+              configurationName = configuration.name,
+              artifact = it.name,
+              version = it.version,
+              fullName = "${it.group}:${it.name}:${it.version}"
+            )
+          }
+      }.distinct()
   }
 
   private fun extractFromModule(module: Project): ModuleStatistics {
