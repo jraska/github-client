73f0bebd10e13b430b1fd56b467507801fff8da3
Josef Raska <6277721+jraska@users.noreply.github.com>
2021-04-09 00:48:06 +0200

    Add a dummy error handler for error cases

--- core-testing/src/main/java/com/jraska/github/client/android/FakeCoreAndroidModule.kt
+++ core-testing/src/main/java/com/jraska/github/client/android/FakeCoreAndroidModule.kt
@@ -7,6 +7,7 @@ import com.jraska.github.client.ui.SnackbarData
 import com.jraska.github.client.ui.SnackbarDisplay
 import dagger.Module
 import dagger.Provides
+import io.reactivex.plugins.RxJavaPlugins
 import javax.inject.Singleton
 
 @Module
@@ -14,6 +15,8 @@ object FakeCoreAndroidModule {
   @Provides
   @Singleton
   fun schedulers(): AppSchedulers {
+    RxJavaPlugins.setErrorHandler { /* empty for now */ } // TODO: 09/04/2021 Better test implementation https://github.com/jraska/github-client/pull/467/checks?check_run_id=2301305103
+
     return Fakes.trampoline()
   }
 
