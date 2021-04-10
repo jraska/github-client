917b95f7cb345e3f342a87b1cab77ba106284547
Josef Raska <6277721+jraska@users.noreply.github.com>
2021-04-10 16:42:03 +0200

    Add reporting ledd time logic

--- plugins/src/main/java/com/jraska/github/client/release/ReleaseMarksPRs.kt
+++ plugins/src/main/java/com/jraska/github/client/release/ReleaseMarksPRs.kt
@@ -1,6 +1,8 @@
 package com.jraska.github.client.release
 
+import com.jraska.analytics.AnalyticsReporter
 import com.jraska.github.client.release.data.GitHubApiFactory
+import com.jraska.github.client.release.data.LeadTimeReporter
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.time.Instant
 
@@ -10,7 +12,9 @@ object ReleaseMarksPRs {
     val release = Release(tag, "https://github.com/jraska/github-client/releases/tag/$tag".toHttpUrl(), Instant.now())
 
     val api = GitHubApiFactory.create(environment)
-    val releaseMarker = ReleaseMarker(api, NotesComposer())
+
+    val leadTimeReporter = LeadTimeReporter(api, release, AnalyticsReporter.create("Lead Time Metrics"))
+    val releaseMarker = ReleaseMarker(api, NotesComposer(), leadTimeReporter)
 
     releaseMarker.markPrsWithMilestone(release)
   }
