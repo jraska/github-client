69ab03b7b058682a2c4050be75c994c9135b11a3
Josef Raska <6277721+jraska@users.noreply.github.com>
2021-04-30 23:00:05 +0200

    Use assets

--- feature/users/src/androidTest/java/com/jraska/github/client/users/test/UsersActivityTest.kt
+++ feature/users/src/androidTest/java/com/jraska/github/client/users/test/UsersActivityTest.kt
@@ -3,14 +3,13 @@ package com.jraska.github.client.users.test
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
-import com.jraska.github.client.http.MockWebServerInterceptor
 import com.jraska.github.client.http.MockWebServerInterceptorRule
 import com.jraska.github.client.users.ui.UsersActivity
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -19,7 +18,8 @@ class UsersActivityTest {
   @get:Rule
   val mockWebServer = MockWebServer()
 
-  @get:Rule val mockWebServerInterceptorRule = MockWebServerInterceptorRule(mockWebServer)
+  @get:Rule
+  val mockWebServerInterceptorRule = MockWebServerInterceptorRule(mockWebServer)
 
   @get:Rule
   val rule = ActivityTestRule(UsersActivity::class.java)
@@ -40,47 +40,11 @@ class UsersActivityTest {
     }
   }
 
-  private fun twoUsersResponse() = MockResponse().setResponseCode(200).setBody(
-    "[\n" +
-      "  {\n" +
-      "    \"login\": \"mojombo\",\n" +
-      "    \"id\": 1,\n" +
-      "    \"node_id\": \"MDQ6VXNlcjE=\",\n" +
-      "    \"avatar_url\": \"https://avatars.githubusercontent.com/u/1?v=4\",\n" +
-      "    \"gravatar_id\": \"\",\n" +
-      "    \"url\": \"https://api.github.com/users/mojombo\",\n" +
-      "    \"html_url\": \"https://github.com/mojombo\",\n" +
-      "    \"followers_url\": \"https://api.github.com/users/mojombo/followers\",\n" +
-      "    \"following_url\": \"https://api.github.com/users/mojombo/following{/other_user}\",\n" +
-      "    \"gists_url\": \"https://api.github.com/users/mojombo/gists{/gist_id}\",\n" +
-      "    \"starred_url\": \"https://api.github.com/users/mojombo/starred{/owner}{/repo}\",\n" +
-      "    \"subscriptions_url\": \"https://api.github.com/users/mojombo/subscriptions\",\n" +
-      "    \"organizations_url\": \"https://api.github.com/users/mojombo/orgs\",\n" +
-      "    \"repos_url\": \"https://api.github.com/users/mojombo/repos\",\n" +
-      "    \"events_url\": \"https://api.github.com/users/mojombo/events{/privacy}\",\n" +
-      "    \"received_events_url\": \"https://api.github.com/users/mojombo/received_events\",\n" +
-      "    \"type\": \"User\",\n" +
-      "    \"site_admin\": false\n" +
-      "  },\n" +
-      "  {\n" +
-      "    \"login\": \"defunkt\",\n" +
-      "    \"id\": 2,\n" +
-      "    \"node_id\": \"MDQ6VXNlcjI=\",\n" +
-      "    \"avatar_url\": \"https://avatars0.githubusercontent.com/u/2?v=4\",\n" +
-      "    \"gravatar_id\": \"\",\n" +
-      "    \"url\": \"https://api.github.com/users/defunkt\",\n" +
-      "    \"html_url\": \"https://github.com/defunkt\",\n" +
-      "    \"followers_url\": \"https://api.github.com/users/defunkt/followers\",\n" +
-      "    \"following_url\": \"https://api.github.com/users/defunkt/following{/other_user}\",\n" +
-      "    \"gists_url\": \"https://api.github.com/users/defunkt/gists{/gist_id}\",\n" +
-      "    \"starred_url\": \"https://api.github.com/users/defunkt/starred{/owner}{/repo}\",\n" +
-      "    \"subscriptions_url\": \"https://api.github.com/users/defunkt/subscriptions\",\n" +
-      "    \"organizations_url\": \"https://api.github.com/users/defunkt/orgs\",\n" +
-      "    \"repos_url\": \"https://api.github.com/users/defunkt/repos\",\n" +
-      "    \"events_url\": \"https://api.github.com/users/defunkt/events{/privacy}\",\n" +
-      "    \"received_events_url\": \"https://api.github.com/users/defunkt/received_events\",\n" +
-      "    \"type\": \"User\",\n" +
-      "    \"site_admin\": true\n" +
-      "  }]"
-  )
+  private fun twoUsersResponse(): MockResponse {
+    val assetsStream = InstrumentationRegistry.getInstrumentation().targetContext.assets.open("users/two_users.json")
+
+    val json = assetsStream.bufferedReader().readText()
+
+    return MockResponse().setResponseCode(200).setBody(json)
+  }
 }
