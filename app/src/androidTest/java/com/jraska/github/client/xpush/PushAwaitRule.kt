ead5a6565d28d213d53409055f7f6e1ef1be1433
Josef Raska <6277721+jraska@users.noreply.github.com>
2021-05-01 11:07:44 +0200

    Test improvements for push tests

--- app/src/androidTest/java/com/jraska/github/client/xpush/PushAwaitRule.kt
+++ app/src/androidTest/java/com/jraska/github/client/xpush/PushAwaitRule.kt
@@ -3,7 +3,6 @@ package com.jraska.github.client.xpush
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.IdlingResource
 import androidx.test.espresso.idling.CountingIdlingResource
-import androidx.test.internal.runner.junit4.statement.UiThreadStatement
 import com.google.firebase.messaging.RemoteMessage
 import com.jraska.github.client.push.PushHandleModel
 import org.junit.rules.ExternalResource
@@ -17,7 +16,7 @@ class PushAwaitRule : ExternalResource() {
     IdlingRegistry.getInstance().unregister(PushAwaitIdlingResource.idlingResource())
   }
 
-  fun viewWillAwaitPush() {
+  fun onViewWillAwaitPush() {
     PushAwaitIdlingResource.waitForPush()
   }
 
@@ -28,16 +27,11 @@ class PushAwaitRule : ExternalResource() {
 
     fun waitForPush() = countingIdlingResource.increment()
 
-    fun onPush() {
-      // Doing this to make sure anything scheduled on UI thread will run before this
-      UiThreadStatement.runOnUiThread {
-        countingIdlingResource.decrement()
-      }
-    }
+    fun onPush() = countingIdlingResource.decrement()
   }
 
   class TestPushHandleModel(
-    val productionModel: PushHandleModel,
+    private val productionModel: PushHandleModel,
   ) : PushHandleModel by productionModel {
     override fun onMessageReceived(message: RemoteMessage) {
       productionModel.onMessageReceived(message)
