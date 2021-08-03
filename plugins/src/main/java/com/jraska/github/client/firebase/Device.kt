b06ce721a74979ef0e44f9beaa5e5724215d5b4a
Josef Raska <6277721+jraska@users.noreply.github.com>
2021-08-03 19:27:22 +0200

    Run on Pixel 5

--- plugins/src/main/java/com/jraska/github/client/firebase/Device.kt
+++ plugins/src/main/java/com/jraska/github/client/firebase/Device.kt
@@ -16,6 +16,6 @@ class Device(
 
   companion object {
     val Pixel4 = Device("flame", 30, "en", "portrait")
-    val Pixel3 = Device("blueline", 28, "en", "portrait")
+    val Pixel5 = Device("redfin", 30, "en", "portrait")
   }
 }
