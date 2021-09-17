7479d7d938695ca402dbd671ec9667417acecae5
Josef Raska <6277721+jraska@users.noreply.github.com>
2021-09-17 16:04:04 +0200

    This commit moves the RepoDetailViewModelTest to fail

--- feature/repo/src/main/java/com/jraska/github/client/repo/model/RepoGitHubApi.kt
+++ feature/repo/src/main/java/com/jraska/github/client/repo/model/RepoGitHubApi.kt
@@ -6,8 +6,8 @@ import retrofit2.http.Path
 
 internal interface RepoGitHubApi {
   @GET("/repos/{owner}/{name}")
-  fun getRepo(@Path("owner") path: String, @Path("name") name: String): Call<GitHubRepo>
+  suspend fun getRepo(@Path("owner") path: String, @Path("name") name: String): GitHubRepo
 
   @GET("/repos/{owner}/{name}/pulls?state=all")
-  fun getPulls(@Path("owner") path: String, @Path("name") name: String): Call<List<GitHubPullRequest>>
+  suspend fun getPulls(@Path("owner") path: String, @Path("name") name: String): List<GitHubPullRequest>
 }
