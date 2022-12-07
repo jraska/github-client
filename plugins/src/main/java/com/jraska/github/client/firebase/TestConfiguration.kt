c7f51e1184ec0a06c2cdfc32fa8d90ee5c29443a
Josef Raska <6277721+jraska@users.noreply.github.com>
2022-12-07 17:49:23 +0000

    Use 3 devices again and get ready for merge

--- plugins/src/main/java/com/jraska/github/client/firebase/TestConfiguration.kt
+++ plugins/src/main/java/com/jraska/github/client/firebase/TestConfiguration.kt
@@ -1,7 +1,5 @@
 package com.jraska.github.client.firebase
 
-import com.jraska.github.client.firebase.Device.Companion.Pixel6a
-import com.jraska.github.client.firebase.Device.Companion.Pixel7Pro
 import org.gradle.api.Project
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -18,7 +16,7 @@ class TestConfiguration(
       val testApk = "${project.buildDir}/outputs/apk/androidTest/debug/app-debug-androidTest.apk"
 
       val resultDir = DateTimeFormatter.ISO_DATE_TIME.format(LocalDateTime.now())
-      val devices = listOf(Pixel7Pro, Pixel6a, Device.Pixel2)
+      val devices = listOf(Device.Pixel7Pro, Device.Pixel6a, Device.Pixel2)
 
       return TestConfiguration(appApk, testApk, devices, resultDir)
     }
