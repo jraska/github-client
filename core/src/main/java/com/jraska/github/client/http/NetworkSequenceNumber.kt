9600e138f6c261dfc1ad64f9ac1a80fb9381c5a6
Josef Raska <6277721+jraska@users.noreply.github.com>
2022-12-15 21:49:41 +0100

    Delete unused class

--- core/src/main/java/com/jraska/github/client/http/NetworkSequenceNumber.kt
+++
@@ -1,9 +0,0 @@
-package com.jraska.github.client.http
-
-import java.util.concurrent.atomic.AtomicInteger
-
-class NetworkSequenceNumber {
-  private val sequenceNumber = AtomicInteger(0)
-
-  fun getNext() = sequenceNumber.incrementAndGet()
-}
